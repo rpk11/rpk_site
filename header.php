df5f9e0d52cfd1e22609fd9f34199e13ad3f24cd
Richie <richiekellyie@gmail.com>
2019-02-27 20:03:50 +0000

    adding dot slash on index link for consistency

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
         <ul id="navbar">
             <li class="navbar-items"><a href="./index.php">Home</a></li>
             <li class="navbar-items"><a href="./index.php#skills">Skills</a></li>
-            <li class="navbar-items"><a href="index.php#process">Process</a></li>
+            <li class="navbar-items"><a href="./index.php#process">Process</a></li>
             <li class="navbar-items"><a href="./index.php#portfolio">Portfolio</a></li>
             <li class="navbar-items"><a href="./index.php#about">About</a></li>
             <li class="navbar-items"><a href="./index.php#contact">Contact</a></li>
